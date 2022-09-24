dc679f5a8d2354c5dd88bcdd51ea94ac919e5368
demyan <demyvn@yandex.ru>
2022-09-24 21:14:48 +0300

    final step создали новый цикл который отделяет подходящие по значению жлементы и выводит их в новый массив

--- Program.cs
+++ Program.cs
@@ -7,3 +7,15 @@ for (int i = 0; i < old.Length; i++)
         count++ ;      
     }
 }
+string[] mem = new string[count];
+int j = 0;
+
+for (int i = 0; i < old.Length; i++)
+{
+    if (old[i].Length <= 3)
+    {
+        mem[j] = old[i];
+        Console.WriteLine($"{mem[j]}, ");
+        j++;
+    }
+}
